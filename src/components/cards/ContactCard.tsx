b64a7c15dce129e13c9034c6fbc70b2fe63606bf
Juan Alberto Palavecino <japc93k@gmail.com>
2025-03-07 01:36:17 +0100

    added animated border

--- src/components/cards/ContactCard.tsx
+++ src/components/cards/ContactCard.tsx
@@ -3,7 +3,7 @@ import { Mail } from 'lucide-react'
 
 export const ContactCard = () => {
   return (
-    <Card className="bg-zinc-900 border-zinc-800">
+    <Card className="[background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.slate.600/.48))_border-box] rounded-2xl border border-transparent animate-border">
       <CardHeader>
         <CardTitle>Get in Touch</CardTitle>
       </CardHeader>
